e1f31b27baba241b18250ea75c2372bf93fc0ddf
yashcoincade <yash@coincade.io>
2022-11-11 14:42:34 +0530

    Added DAO Admin function to get owners

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import{ethers, BigNumber} from 'ethers';
 import { useContract, useContractRead, useContractWrite } from "@thirdweb-dev/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
@@ -11,295 +11,297 @@ export default function Home() {
   const [proposalId, setProposalId] = useState("")
   const [contractBal, setContractBal] = useState(0)
   let NFTOwners = [];
+  let finalNFTOwners = [];
   const currency= "MATIC"
+  const tokenId = "2"
 
-  const DaoAddress = "0x7E08c5E0D1548d4b100e3137F91A8339C4d4AA01";
-  const DaoAbi = [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "votesUp",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "votesDown",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "voter",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "proposal",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "bool",
-          "name": "votedFor",
-          "type": "bool"
-        }
-      ],
-      "name": "newVote",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "id",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "bool",
-          "name": "passed",
-          "type": "bool"
-        }
-      ],
-      "name": "proposalCount",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "id",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "description",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "maxVotes",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "proposer",
-          "type": "address"
-        }
-      ],
-      "name": "proposalCreated",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "Proposals",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "id",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bool",
-          "name": "exists",
-          "type": "bool"
-        },
-        {
-          "internalType": "string",
-          "name": "description",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "deadline",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "votesUp",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "votesDown",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "maxVotes",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bool",
-          "name": "countConducted",
-          "type": "bool"
-        },
-        {
-          "internalType": "bool",
-          "name": "passed",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_tokenId",
-          "type": "uint256"
-        }
-      ],
-      "name": "addTokenId",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_id",
-          "type": "uint256"
-        }
-      ],
-      "name": "countVotes",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_description",
-          "type": "string"
-        },
-        {
-          "internalType": "address[]",
-          "name": "_canVote",
-          "type": "address[]"
-        }
-      ],
-      "name": "createProposal",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getValidTokens",
-      "outputs": [
-        {
-          "internalType": "uint256[]",
-          "name": "",
-          "type": "uint256[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "nextProposal",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "owner",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_nftAddress",
-          "type": "address"
-        }
-      ],
-      "name": "setNFTAddress",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "validTokens",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_id",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bool",
-          "name": "_vote",
-          "type": "bool"
-        }
-      ],
-      "name": "voteOnProposal",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ]
+  const DaoAddress = "0xd3ac637eb34246d4b6A4b0084759E27b51b1416D";
+  // const DaoAbi = [
+  //   {
+  //     "inputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "constructor"
+  //   },
+  //   {
+  //     "anonymous": false,
+  //     "inputs": [
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "votesUp",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "votesDown",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "address",
+  //         "name": "voter",
+  //         "type": "address"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "proposal",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "bool",
+  //         "name": "votedFor",
+  //         "type": "bool"
+  //       }
+  //     ],
+  //     "name": "newVote",
+  //     "type": "event"
+  //   },
+  //   {
+  //     "anonymous": false,
+  //     "inputs": [
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "id",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "bool",
+  //         "name": "passed",
+  //         "type": "bool"
+  //       }
+  //     ],
+  //     "name": "proposalCount",
+  //     "type": "event"
+  //   },
+  //   {
+  //     "anonymous": false,
+  //     "inputs": [
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "id",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "string",
+  //         "name": "description",
+  //         "type": "string"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "uint256",
+  //         "name": "maxVotes",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "indexed": false,
+  //         "internalType": "address",
+  //         "name": "proposer",
+  //         "type": "address"
+  //       }
+  //     ],
+  //     "name": "proposalCreated",
+  //     "type": "event"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "name": "Proposals",
+  //     "outputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "id",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "bool",
+  //         "name": "exists",
+  //         "type": "bool"
+  //       },
+  //       {
+  //         "internalType": "string",
+  //         "name": "description",
+  //         "type": "string"
+  //       },
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "deadline",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "votesUp",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "votesDown",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "maxVotes",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "bool",
+  //         "name": "countConducted",
+  //         "type": "bool"
+  //       },
+  //       {
+  //         "internalType": "bool",
+  //         "name": "passed",
+  //         "type": "bool"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "_tokenId",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "name": "addTokenId",
+  //     "outputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "_id",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "name": "countVotes",
+  //     "outputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "string",
+  //         "name": "_description",
+  //         "type": "string"
+  //       },
+  //       {
+  //         "internalType": "address[]",
+  //         "name": "_canVote",
+  //         "type": "address[]"
+  //       }
+  //     ],
+  //     "name": "createProposal",
+  //     "outputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [],
+  //     "name": "getValidTokens",
+  //     "outputs": [
+  //       {
+  //         "internalType": "uint256[]",
+  //         "name": "",
+  //         "type": "uint256[]"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [],
+  //     "name": "nextProposal",
+  //     "outputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [],
+  //     "name": "owner",
+  //     "outputs": [
+  //       {
+  //         "internalType": "address",
+  //         "name": "",
+  //         "type": "address"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "address",
+  //         "name": "_nftAddress",
+  //         "type": "address"
+  //       }
+  //     ],
+  //     "name": "setNFTAddress",
+  //     "outputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "name": "validTokens",
+  //     "outputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   },
+  //   {
+  //     "inputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "_id",
+  //         "type": "uint256"
+  //       },
+  //       {
+  //         "internalType": "bool",
+  //         "name": "_vote",
+  //         "type": "bool"
+  //       }
+  //     ],
+  //     "name": "voteOnProposal",
+  //     "outputs": [],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   }
+  // ]
   const NftAddress= "0x7133a16062C5A9059d35706518e94D1Fb726Ff6f";
   const NftAbi = [
     {
@@ -1145,7 +1147,7 @@ export default function Home() {
   
 
 
-  const { contract } = useContract(DaoAddress, DaoAbi);
+  const { contract } = useContract(DaoAddress);
   const { mutateAsync: createProposal } = useContractWrite(contract, "createProposal");
   const { mutateAsync: countVotes } = useContractWrite(contract, "countVotes");
 
@@ -1159,12 +1161,16 @@ export default function Home() {
   };
   const alchemy = new Alchemy(settings);
   const getNftOwners = async () => {
-    const ownerAccounts = await alchemy.nft.getOwnersForNft("0xb4EDF0E780aCd479B6f051374a50d25E830f1557", "0")
-    console.log(ownerAccounts.owners[0]);
-    for (let i = 0; i < ownerAccounts.owners.length; i++) {
-      NFTOwners.push(ownerAccounts.owners[i]);
+    for(let j=0; j<= tokenId; j++){
+      const ownerAccounts = await alchemy.nft.getOwnersForNft(NftAddress, tokenId)
+      // console.log(ownerAccounts.owners[0]);
+      for (let i = 0; i < ownerAccounts.owners.length; i++) {
+        NFTOwners.push(ownerAccounts.owners[i]);
+      }
     }
     console.log(NFTOwners);
+    finalNFTOwners = [...new Set(NFTOwners)];
+    console.log(finalNFTOwners);
   }
 
 
@@ -1188,7 +1194,7 @@ export default function Home() {
     const notification = toast.loading("Creating Proposal...");
     await getNftOwners();
     try {
-      const data = await createProposal([description, NFTOwners]);
+      const data = await createProposal([description, finalNFTOwners]);
       console.info("contract call successs", data);
       toast.success("Proposal Created Successfully!", {
         id: notification,
@@ -1223,6 +1229,10 @@ export default function Home() {
     console.log((balance/1e18).toString());
     setContractBal((balance/1e18).toString());
   }
+  useEffect(() => {
+    getBalance();
+  }, [])
+  
   getBalance();
 
 
@@ -1243,7 +1253,7 @@ export default function Home() {
         <p className='pb-1 text-2xl'>Description:</p>
         <input type="text" placeholder=" Enter Description Here" onChange={(e) => { setDescription(e.target.value) }} className=" m-1 rounded-lg border border-b-gray-900 lg:w-[1000px] text-black text-2xl" />
         <button className="border-2 rounded-sm p-1 bg-[#256D85]" onClick={handleCreateProposal}>Create Proposal</button>
-        {/* <button onClick={getNftOwners} className="border-2 rounded-sm p-1 bg-[#256D85] ">Get Owners</button> */}
+        <button onClick={getNftOwners} className="border-2 rounded-sm p-1 bg-[#256D85] ">Get Owners</button>
       </div>
 
       <div className="cont">
